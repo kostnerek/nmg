419f081edc13c2269bbaef7ae582be8ff6cdfd2c
Filip Kostecki <filip.kostecki00@gmail.com>
2024-08-05 22:10:14 +0200

    feat: add multisegment module name

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # nestjs-module-generator
 
 Custom module generator for applications using NestJS, mongoose and GraphQL.
+
+Use this generator to create a new module with a model, service, resolver and input type.
+
+It will create the following files:
+```bash npx nestjs-mg --name=test --rootDir=src```
+```bash
+src/
+└── test
+    ├── errors
+    │   ├── graphql-test-errors.ts
+    │   └── test-not-found.error.ts
+    ├── inputs
+    │   └── test.input.ts
+    ├── models
+    │   └── test.model.ts
+    ├── schemas
+    │   └── test.schema.ts
+    ├── test.module.ts
+    ├── test.repository.ts
+    ├── test.resolver.ts
+    └── test.service.ts
+```
--- index.js
+++ index.js
@@ -1,19 +1,48 @@
 const fs = require('node:fs');
 const path = require('node:path');
-
 const { program } = require('commander');
 const Handlebars = require('handlebars');
+
+const normalizeString = (sourceStr) => {
+  const charsToSplitAt = ['_', '-']
+
+  let resultStr=''
+  for(let i=0;i<sourceStr.length;i++) {
+    const char = sourceStr[i]
+    if(charsToSplitAt.includes(char)) {
+      resultStr+=sourceStr[i+1].toUpperCase()
+      i++
+    } else {
+      resultStr+=char
+    }
+    
+  }
+  return resultStr;
+}
+
 
 program
   .option('-n, --name <name>', 'Name of the module')
   .option(
     '-d, --rootDir <name>',
     'Name of your root directory, default is src',
-  );
+);
 program.parse(process.argv);
 const options = program.opts();
+const moduleName = options.name.toLowerCase();
 
-const moduleName = options.name;
+/* 
+
+  errorName - MODULE_NAME
+  className - ModuleName
+  variables - moduleName
+  pathName  - module-name
+*/
+
+const errorName = moduleName.replace("-", "_").toUpperCase()
+const variableName = normalizeString(moduleName)
+const className = variableName.charAt(0).toUpperCase() + variableName.slice(1)
+const pathName = moduleName
 
 const rootPath = options.rootDir || 'src';
 
@@ -30,52 +59,30 @@ const fileNames = {
   service: {path: `${folderNames.rootModulePath}/${moduleName}.service.ts`, template: 'service.hbs'},
   repository: {path: `${folderNames.rootModulePath}/${moduleName}.repository.ts`, template: 'repository.hbs'},
   resolver: {path: `${folderNames.rootModulePath}/${moduleName}.resolver.ts`, template: 'resolver.hbs'},
-
   graphqlError: {path: `${folderNames.errors}/graphql-${moduleName}-errors.ts`, template: 'graphql-error.hbs'},
   graphql: {path: `${folderNames.errors}/${moduleName}-not-found.error.ts`, template: 'error.hbs'},
-
   input: {path: `${folderNames.inputs}/${moduleName}.input.ts`, template: 'input.hbs'},
-
   schema: {path: `${folderNames.schemas}/${moduleName}.schema.ts`, template: 'schema.hbs'},
-
   model: {path: `${folderNames.models}/${moduleName}.model.ts`, template: 'model.hbs'},
 };
 
-//TODO: UNCOMMENT
-/* if (fs.existsSync(folderNames.rootModulePath)) {
+if (fs.existsSync(folderNames.rootModulePath)) {
   console.error('Module already exists');
   process.exit(1);
-} */
+}
 
 for (const folder in folderNames) {
   fs.mkdirSync(folderNames[folder], { recursive: true });
 }
 
-Handlebars.registerHelper('toUpperCase', function (str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-});
-
-Handlebars.registerHelper('toFullUpperCase', function (str) {
-  return str.toUpperCase();
-});
-
-
-
 for(const file in fileNames) {
-  console.log(fileNames[file]);
   const { path: filePath, template } = fileNames[file];
   const templateContent = fs.readFileSync(
     path.join(__dirname, 'templates', template),
     'utf8',
   );
   const compiledTemplate = Handlebars.compile(templateContent);
-  fs.writeFileSync(filePath, compiledTemplate({ moduleName }));
+  fs.writeFileSync(filePath, compiledTemplate({ moduleName, errorName, variableName, className, pathName }));
 }
-/* const template = Handlebars.compile(
-  fs.readFileSync(
-    path.join(__dirname, 'templates', fileNames.module.template),
-    'utf8',
-  ),
-);
-console.log(template({ moduleName })); */
+console.log(`Module [${moduleName}] created successfully`);
 
